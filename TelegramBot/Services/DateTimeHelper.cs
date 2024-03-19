1ee76eaa0a8726983cc3aeba732b8baad386ef73
Hu Tao <zakhar.yagodin@yandex.ru>
2024-03-19 21:49:40 +0300

    fixed cultureinfo

--- TelegramBot/Services/DateTimeHelper.cs
+++ TelegramBot/Services/DateTimeHelper.cs
@@ -7,24 +7,19 @@ public static class DateTimeHelper
     private static readonly string[] Formats = { "HH:MM", "HH:MM:SS" };
 
     public static DateTime DateTimeFromString(this string dateTimeObject)
-        => DateTimeFromString(dateTimeObject, CultureInfo.InvariantCulture);
+        => DateTimeFromString(dateTimeObject, null);
 
-    public static DateTime DateTimeFromString(this string dateTimeObject, CultureInfo cultureInfo)
+    public static DateTime DateTimeFromString(this string dateTimeObject, CultureInfo? cultureInfo)
     {
         if (int.TryParse(dateTimeObject, out int fromIntValue))
         {
-            if (fromIntValue <= 0) throw new ArgumentException("DateTime must be positive");
+            if (fromIntValue <= 0) throw new FormatException("DateTime must be positive");
             return DateTime.Now.AddSeconds(fromIntValue);
         }
 
         Console.WriteLine(dateTimeObject);
 
-        if (!TimeOnly.TryParseExact(
-                dateTimeObject,
-                Formats,
-                cultureInfo,
-                DateTimeStyles.None,
-                out TimeOnly fromTimeOnlyValue)) throw new ArgumentException("Invalid format of time");
+        var fromTimeOnlyValue = TimeOnly.ParseExact(dateTimeObject, Formats, cultureInfo);
 
         var value = new DateTime(DateOnly.FromDateTime(DateTime.Now), fromTimeOnlyValue);
         if (value < DateTime.Now) value = value.AddDays(1);
